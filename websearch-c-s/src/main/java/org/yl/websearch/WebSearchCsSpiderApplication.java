98693da1fcfa8d8afc735a806b7da4c44f61306e
liuyang <Hello006>
2020-09-29 13:36:22 +0800

    搭建springboot启动环境，规定工具类

---
+++ websearch-c-s/src/main/java/org/yl/websearch/WebSearchCsSpiderApplication.java
@@ -0,0 +1,17 @@
+package org.yl.websearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yang.liu
+ * @createtime 2020/9/29 12:57
+ * @description 启动类
+ */
+@SpringBootApplication
+public class WebSearchCsSpiderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSearchCsSpiderApplication.class, args);
+    }
+}
