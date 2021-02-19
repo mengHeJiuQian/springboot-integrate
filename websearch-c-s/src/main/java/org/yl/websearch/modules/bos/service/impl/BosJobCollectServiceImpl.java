ca56e9a975b05a4280a2526aa11ab08a11dd9a8b
liuyang <1197563121@qq.com>
2021-02-19 21:05:19 +0800

    设置临时cookie

--- websearch-c-s/src/main/java/org/yl/websearch/modules/bos/service/impl/BosJobCollectServiceImpl.java
+++ websearch-c-s/src/main/java/org/yl/websearch/modules/bos/service/impl/BosJobCollectServiceImpl.java
@@ -1,17 +1,13 @@
 package org.yl.websearch.modules.bos.service.impl;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
+import java.util.HashMap;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.codec.digest.DigestUtils;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.springframework.stereotype.Service;
 import org.yl.websearch.modules.bos.service.BosJobCollectService;
-import org.yl.websearch.modules.cs.model.CsUrlInfo;
 
 /**
  * @author yang.liu
@@ -25,6 +21,8 @@ public class BosJobCollectServiceImpl implements BosJobCollectService {
     private static String beginUrl
             = "https://www.zhipin.com/job_detail/?query=golang&city=101180700&industry=&position=";
 
+    private static String COOKIE = "lastCity=101020100; __zp_seo_uuid__=b1a9841b-88c6-4404-94da-9e253f3f1ad0; __g=-; __l=r=https%3A%2F%2Fwww.baidu.com%2Flink%3Furl%3D086t6tnhof7KJeFnHmhEHWqhl8UMQJP7XE3qeP6pEBY5sLTbXELvuYc-MOtKeuai%26ck%3D3638.3.88.931.517.846.517.306%26shh%3Dwww.baidu.com%26sht%3Dmonline_7_dg%26wd%3D%26eqid%3Dd32a999a000fc79e00000003602fac9d&l=%2Fwww.zhipin.com%2Fshanghai%2F&s=1&g=&s=3&friend_source=0; __fid=7159290f1132451d31967735cc862733; __zp_stoken__=5bbdbaR9wOxkrA0BXBHgUKl1EGhQ3Q2RnW2FVcxp8NicpO3x5bTdifjRMUnsLBREhAh8SLigvKHlVEncdfHB4P3tSVWAiUzQ8Y2ALaS9ncy5vE3pJTU0CfgkRFkFNfCobBE4YFztOewZUb2l%2BYQ%3D%3D; __c=1613737125; __a=84961833.1613654747.1613654747.1613737125.12.2.4.12";
+
     @Override
     public void collectJobInArea() {
         Integer jobCount = collectUrlFromPage(beginUrl);
@@ -35,30 +33,26 @@ public class BosJobCollectServiceImpl implements BosJobCollectService {
         Integer jobCount = 0;
         try {
             Connection connect = Jsoup.connect(url);
+            connect.cookies(convertCookie(COOKIE));
             Document document = connect.get();
-            String htmlText = document.toString();
-            log.info("--------");
-            log.info(htmlText);
-            log.info("--------");
-//            Matcher matcher = urlPattern.matcher(htmlText);
-//            urlInfoList = new ArrayList<>();
-//            while (matcher.find()) {
-//                String group = matcher.group();
-//                // 去掉头尾的引号
-//                String urlStr = group.substring(1, group.length() - 1);
-//                String id = DigestUtils.md5Hex(urlStr);
-//                CsUrlInfo urlInfo = new CsUrlInfo(id, urlStr, LocalDateTime.now());
-//                if (isUselessUrl(urlInfo.getUrl())) {
-//                    int count = csUrlInfoDaoImpl.deleteByUrl(urlStr);
-//                    log.info("【删除url={}，影响行数={}】。", urlStr, count);
-//                    continue;
-//                }
-//                urlInfoList.add(urlInfo);
-//            }
+            Element element = document.selectFirst("#job-tab");
+            String dataKeyword = element.attr("data-keyword");
+            String dataResCount = element.attr("data-rescount");
+            log.info("-----dataKeyword={}", dataKeyword);
+            log.info("-----dataResCount={}", dataResCount);
         } catch (Exception e) {
             log.error("【异常处理，获取url={}出现异常】：{}", url, e);
         }
         return jobCount;
+    }
+
+    private HashMap<String, String> convertCookie(String cookie) {
+        HashMap<String, String> cookiesMap = new HashMap<>();
+        String[] items = cookie.trim().split(";");
+        for (String item : items) {
+            cookiesMap.put(item.split("=")[0], item.split("=")[1]);
+        }
+        return cookiesMap;
     }
 
 }
